70897973ae71765f99c9120054da5d0c10b43608
imckain <ianmmckain@gmail.com>
2021-05-01 19:29:35 -0400

    side menu built and functioning, UI design implemented

--- Poke-Team/MainWindow.xaml.cs
+++ Poke-Team/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Poke_Team
 {
@@ -20,9 +21,57 @@ namespace Poke_Team
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+
+        double panelWidth;
+        bool hidden;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer {Interval = new TimeSpan(0, 0, 0, 0, 0)};
+            timer.Tick += Timer_Tick;
+
+            panelWidth = SidePanel.Width;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                SidePanel.Width += 1;
+                if (SidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                SidePanel.Width -= 1;
+                if (SidePanel.Width <= 55)
+                {
+                    timer.Stop();
+                    hidden = true;
+                }
+            }
+        }
+
+        private void Close_Button(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PanelHeader_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Menu_Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
